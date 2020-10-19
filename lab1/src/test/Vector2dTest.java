e430ed24d6a103c5a5771c4d1d59e34c5b532cf1
szymonSumara <szymon.sumara.zst@gmail.com>
2020-10-19 21:53:29 +0200

    Rewrite some tests (Vector2d)

--- lab1/src/test/Vector2dTest.java
+++ lab1/src/test/Vector2dTest.java
@@ -2,151 +2,54 @@ package test;
 import agh.cs.lab1.Vector2d;
 import org.junit.jupiter.api.Test;
 
-import java.util.ArrayList;
-import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
-import org.junit.jupiter.api.Test;
-import static org.junit.jupiter.api.Assertions.fail;
 public class Vector2dTest {
 
     @Test
     public void equalsTest(){
-        boolean everythinkIsOk = true;
-        System.out.println("______________________ \nVector2.equal()  \n----------------------");
+
         Vector2d vec1 = new Vector2d(1,2);
         Vector2d vec2 = new Vector2d(1,2);
         Vector2d vec3 = new Vector2d(4,3);
         Object vec4 = new Object();
 
-        if(vec1.equals(vec1) == true){
-            System.out.println(" Test 1: ok");
-        }
-        else {
-            System.out.println(" Test 1: falirue");
-            everythinkIsOk = false;
-        }
-
-        if(vec1.equals(vec2) == true){
-            System.out.println(" Test 2: ok");
-        }
-        else {
-            System.out.println(" Test 2: falirue");
-            everythinkIsOk = false;
-        }
-
-        if(vec1.equals(vec3) == false){
-            System.out.println(" Test 3: ok");
-        }
-        else {
-            System.out.println(" Test 3: falirue");
-            everythinkIsOk = false;
-        }
-
-        if(vec1.equals(vec4) == false){
-            System.out.println(" Test 4: ok");
-        }
-        else {
-            System.out.println(" Test 4: falirue");
-            everythinkIsOk = false;
-        }
-        if(!everythinkIsOk)
-            fail();
-        System.out.println("______________________");
+        assertEquals(true,vec1.equals(vec2));
+        assertEquals(false,vec2.equals(vec3));
+        assertEquals(false,vec3.equals(vec4));
 
     }
     @Test
     public void toStringTest(){
-        boolean everythinkIsOk = true;
-        System.out.println("______________________ \nVector2d.toString()  \n----------------------");
+
         Vector2d vec1 = new Vector2d(1,2);
         Vector2d vec2 = new Vector2d(89,-10);
         Vector2d vec3 = new Vector2d(-123,0);
         Vector2d vec4 = new Vector2d(-9999,-9999);
 
-        if(vec1.toString().equals("(1,2)")){
-            System.out.println(" Test 1: ok");
-        }
-        else {
-            System.out.println(" Test 1: falirue");
-            everythinkIsOk = false;
-        }
 
-        if(vec2.toString().equals( "(89,-10)")){
-            System.out.println(" Test 2: ok");
-        }
-        else {
-            System.out.println(" Test 2: falirue");
-            everythinkIsOk = false;
-        }
-
-        if(vec3.toString().equals("(-123,0)")){
-            System.out.println(" Test 3: ok");
-        }
-        else {
-            System.out.println(" Test 3: falirue");
-            everythinkIsOk = false;
-        }
-
-        if(vec4.toString().equals("(-9999,-9999)")){
-            System.out.println(" Test 4: ok");
-        }
-        else {
-            System.out.println(" Test 4: falirue");
-            everythinkIsOk = false;
-        }
-        if(!everythinkIsOk)
-            fail();
-        System.out.println("______________________");
+        assertEquals("(1,2)",vec1.toString());
+        assertEquals("(89,-10)",vec2.toString());
+        assertEquals("(-123,0)",vec3.toString());
+        assertEquals("(-9999,-9999)",vec4.toString());
 
     }
 
     @Test
     public void precedesTest(){
-        boolean everythinkIsOk = true;
-        List<Vector2d> testsVectors =   new ArrayList<Vector2d>();
-        testsVectors.add(new Vector2d(1,2));
-        testsVectors.add(new Vector2d(7,8));
-        testsVectors.add(new Vector2d(-1,10));
-        testsVectors.add(new Vector2d(-20,8));
-        testsVectors.add(new Vector2d(7,8));
-        System.out.println("______________________ \nVector2d.precedes()  \n----------------------");
-        if(testsVectors.get(0).precedes(testsVectors.get(1)) == true){
-            System.out.println(" Test 1: ok");
-        }
-        else{
-            System.out.println(" Test 1: falirue");
-            everythinkIsOk = false;
-        }
 
-        if(testsVectors.get(1).precedes(testsVectors.get(1)) == true){
-            System.out.println(" Test 2: ok");
-        }
-        else{
-            System.out.println(" Test 2: falirue");
-            everythinkIsOk = false;
-        }
+        Vector2d vec1 = new Vector2d(1,2);
+        Vector2d vec2 = new Vector2d(7,8);
+        Vector2d vec3 = new Vector2d(-1,10);
+        Vector2d vec4 = new Vector2d(-20,8);
+        Vector2d vec5 = new Vector2d(7,8);
 
-        if(testsVectors.get(3).precedes(testsVectors.get(1)) == true){
-            System.out.println(" Test 3: ok");
-        }
-        else{
-            System.out.println(" Test 3: falirue");
-            everythinkIsOk = false;
-        }
+        assertEquals(true,vec1.precedes(vec2));
+        assertEquals(true,vec2.precedes(vec2));
+        assertEquals(true,vec4.precedes(vec2));
 
-        if(testsVectors.get(3).precedes(testsVectors.get(0)) == false){
-            System.out.println(" Test 4: ok");
-        }
-        else{
-            System.out.println(" Test 4: falirue");
-            everythinkIsOk = false;
-        }
-
-        if(!everythinkIsOk)
-            fail();
-        System.out.println("______________________");
+        assertEquals(false,vec4.precedes(vec1));
 
     }
 
@@ -227,8 +130,6 @@ public class Vector2dTest {
         assertEquals(new Vector2d(-3,-4),vec2.opposite());
         assertEquals(new Vector2d(0,0),vec3.opposite());
         assertEquals(new Vector2d(-132,123),vec4.opposite());
-
-
 
     }
 
